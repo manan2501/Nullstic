623d0ddba8ef83ed08f4390657c81f708eeb9e93
manan2501 <kevadiyamanan@gmail.com>
2019-12-30 17:29:55 +0530

    Updates

---
+++ precache-manifest.30a7f05dd17bdc581df7ae51cad3df6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16d8364d713ceab74a13de10ec4d567",
+    "url": "/Nullstic/index.html"
+  },
+  {
+    "revision": "ff503248cf68468974bb",
+    "url": "/Nullstic/static/css/2.b4622e8d.chunk.css"
+  },
+  {
+    "revision": "0211c71985d630ebbc28",
+    "url": "/Nullstic/static/css/main.0784a744.chunk.css"
+  },
+  {
+    "revision": "ff503248cf68468974bb",
+    "url": "/Nullstic/static/js/2.38498d5b.chunk.js"
+  },
+  {
+    "revision": "0211c71985d630ebbc28",
+    "url": "/Nullstic/static/js/main.4836241b.chunk.js"
+  },
+  {
+    "revision": "5ad97c4a4a88122f2c5d",
+    "url": "/Nullstic/static/js/runtime-main.5b4e4971.js"
+  }
+]);
